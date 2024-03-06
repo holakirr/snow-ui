ebeca01ed1b98812591388df3528cf0a660655c5
Kirill Petunin <kpetunin@proton.me>
2024-03-06 17:40:20 -0600

    feat: continues configuring project

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,12 +3,13 @@ import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
 	stories: ["../stories/**/*.story.@(js|jsx|mjs|ts|tsx)"],
 	addons: [
-		"@storybook/addon-onboarding",
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@chromatic-com/storybook",
-		"@storybook/addon-interactions",
-	],
+        "@storybook/addon-onboarding",
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@chromatic-com/storybook",
+        "@storybook/addon-interactions",
+        "@storybook/addon-themes"
+    ],
 	framework: {
 		name: "@storybook/nextjs",
 		options: {},

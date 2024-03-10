3a8ac5e0ae546e6d36ab2811e36aecc284e553cf
Kirill Petunin <kpetunin@proton.me>
2024-03-10 08:53:37 -0600

    feat: adds .cache to .gitignore, adds pre-commit hook, updates storybook preview parameters, remove sunused code, updates README.md, fixes imports, updates vite.config.ts, updates .eslintrc.json, removes vite.config.js, updates .storybook/main.ts, adds .github/workflows/storybook.yml

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+import { peerDependencies } from "./package.json";
+
+export default defineConfig({
+	plugins: [react(), dts({ rollupTypes: true })],
+	build: {
+		target: "esnext",
+		minify: false,
+		lib: {
+			entry: resolve(__dirname, "src/components/index.ts"),
+			fileName: "kage-ui",
+			name: "KageUI",
+		},
+		rollupOptions: {
+			external: ["react/jsx-runtime", ...Object.keys(peerDependencies)],
+		},
+	},
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "src"),
+		},
+	},
+});

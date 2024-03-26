ec27e6af018c9271794586001c0b70e38450605e
holakirr <kpetunin@icloud.com>
2024-03-25 22:24:06 -0600

    feat: update pre-commit hook & update Input component to include icon selection

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -6,6 +6,8 @@ type InputProps = ComponentProps<"input"> & {
 	icon?: CustomIcon;
 };
 
+// TODO: Add content component
+
 const Input = forwardRef<HTMLInputElement, InputProps>(
 	({ placeholder, id, className, icon: Icon, ...props }, ref) => (
 		<div className="relative">
@@ -14,11 +16,18 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
 				placeholder={placeholder}
 				className={twMerge(
 					"px-5 py-4 transition-all bg-white-80 rounded-2xl border-1 border-black-10 placeholder:text-black-20 hover:border-black-40 disabled:bg-black-5 disabled:text-black-10 focus:shadow-black-5 focus:shadow-[0_0_0_4px] focus:outline-none",
+					Icon && "pl-11",
 					className,
 				)}
 				{...props}
 			/>
-			{Icon && <Icon alt={placeholder || "Input icon"} className="absolute" />}
+			{Icon && (
+				<Icon
+					alt={placeholder || "Input icon"}
+					className="absolute top-1/2 left-5 transform -translate-y-1/2"
+					size={20}
+				/>
+			)}
 		</div>
 	),
 );

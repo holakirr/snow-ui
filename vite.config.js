9ad571511c9f7635639321d5a18b9e5dd7b97a4b
holakirr <kpetunin@icloud.com>
2024-03-29 17:46:11 -0600

    feat: Update dependencies in vite.config.js and package.json

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import MillionLint from "@million/lint";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";

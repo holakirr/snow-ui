99b3ae8f4e23bc9de118583ab7a82e72c89f482f
holakirr <kpetunin@icloud.com>
2024-04-09 14:49:28 -0600

    fix: fixes four leaf clover icon types

--- src/components/Icons/ssr/FourLeafClover.tsx
+++ src/components/Icons/ssr/FourLeafClover.tsx
@@ -1,7 +1,7 @@
 import { forwardRef } from "react";
+import type { CustomIcon } from "../../../utils";
 import { FourLeafCloverWeights } from "../defs";
 import { CustomIconBase } from "../lib";
-import type { CustomIcon } from "../types";
 
 const FourLeafCloverIcon: CustomIcon = forwardRef((props, ref) => (
 	<CustomIconBase ref={ref} {...props} weights={FourLeafCloverWeights} />

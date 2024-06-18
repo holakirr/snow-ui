c7a1427957bdeb0a279d8aa2956de845cdb061d2
holakirr <kpetunin@icloud.com>
2024-06-17 20:48:47 -0600

    chore: Update Abbr component in stories

--- stories/components/Abbr.stories.ts
+++ stories/components/Abbr.stories.ts
@@ -26,7 +26,7 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const ButtonWithTooltip: Story = {
+export const BasicAbbr: Story = {
 	args: {},
 	play: async ({ canvasElement }) => {
 		const canvas = within(canvasElement);

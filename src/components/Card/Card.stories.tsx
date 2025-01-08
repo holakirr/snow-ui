a36f414bb3012f79e8045beb5ace9fca2297e34e
Kirill Petunin <kpetunin@icloud.com>
2025-01-08 15:45:52 +0300

    feat: update Card stories to include accessibility tags and simplify Default export

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -7,7 +7,7 @@ import { Card } from './Card'
 const meta: Meta<typeof Card> = {
   title: 'Components/Card',
   component: Card,
-  tags: ['autodocs'],
+  tags: ['autodocs', 'a11y'],
   argTypes: {
     bordered: {
       control: {
@@ -19,34 +19,24 @@ const meta: Meta<typeof Card> = {
     bordered: false,
     className: 'w-[350px] flex flex-col gap-2 items-start',
   },
+  render: (args) => (
+    <Card {...args}>
+      <Input placeholder="Email" name="email" />
+      <Input placeholder="Password" type="password" name="password" />
+      <Button variant="filled" size="lg">
+        Login
+      </Button>
+    </Card>
+  ),
 }
 
 export default meta
 type Story = StoryObj<typeof Card>
 
-export const Default: Story = {
-  render: (args) => (
-    <Card {...args}>
-      <Input placeholder="Email" name="email" />
-      <Input placeholder="Password" type="password" name="password" />
-      <Button variant="filled" size="lg">
-        Login
-      </Button>
-    </Card>
-  ),
-}
+export const Default: Story = {}
 
 export const BorderedCard: Story = {
   args: {
     bordered: true,
   },
-  render: (args) => (
-    <Card {...args}>
-      <Input placeholder="Email" name="email" />
-      <Input placeholder="Password" type="password" name="password" />
-      <Button variant="filled" size="lg">
-        Login
-      </Button>
-    </Card>
-  ),
 }

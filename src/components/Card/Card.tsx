32739acd0b6e2fc7eb4588d40b52235f61ec32ed
Kirill Petunin <kpetunin@icloud.com>
2025-01-08 15:43:49 +0300

    feat: enhance Card component accessibility with role and aria-label

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,7 @@
 import { type VariantProps, cva } from 'class-variance-authority'
 import type { ComponentProps, FC } from 'react'
 import { twMerge } from 'tailwind-merge'
+import { ROLES } from '../../constants'
 
 const cardStyles = cva('rounded-2xl bg-bg5 text-black p-6', {
   variants: {
@@ -17,7 +18,12 @@ const cardStyles = cva('rounded-2xl bg-bg5 text-black p-6', {
 type CardProps = ComponentProps<'div'> & VariantProps<typeof cardStyles>
 
 const Card: FC<CardProps> = ({ bordered, className, ...props }) => (
-  <div className={twMerge(cardStyles({ bordered, className }))} {...props} />
+  <div
+    role={ROLES.region}
+    aria-label="card"
+    className={twMerge(cardStyles({ bordered, className }))}
+    {...props}
+  />
 )
 Card.displayName = 'Card'
 

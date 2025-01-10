c053d078039e89096cc642e0be5d88892a531381
Kirill Petunin <kpetunin@icloud.com>
2025-01-10 06:28:28 +0300

    refactor: reorder imports and format Breadcrumb component stories

--- src/components/Breadcrumb/Breadcrumb.stories.tsx
+++ src/components/Breadcrumb/Breadcrumb.stories.tsx
@@ -2,126 +2,126 @@ import { ArrowLineRightIcon } from '@holakirr/snow-ui-icons'
 import type { Meta, StoryObj } from '@storybook/react'
 
 import {
-	Breadcrumb,
-	BreadcrumbEllipsis,
-	BreadcrumbItem,
-	BreadcrumbLink,
-	BreadcrumbList,
-	BreadcrumbSeparator,
+  Breadcrumb,
+  BreadcrumbEllipsis,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
 } from './Breadcrumb'
 
 const meta: Meta<typeof Breadcrumb> = {
-	title: 'Components/Breadcrumb',
-	component: Breadcrumb,
-	tags: ['autodocs'],
-	args: {
-		children: (
-			<BreadcrumbList>
-				<BreadcrumbItem>
-					<BreadcrumbLink href="/">Home</BreadcrumbLink>
-				</BreadcrumbItem>
-				<BreadcrumbSeparator />
-				<BreadcrumbItem>
-					<BreadcrumbLink href="/docs">Documentation</BreadcrumbLink>
-				</BreadcrumbItem>
-				<BreadcrumbSeparator />
-				<BreadcrumbItem>
-					<BreadcrumbLink href="/docs/components">Components</BreadcrumbLink>
-				</BreadcrumbItem>
-				<BreadcrumbSeparator />
-				<BreadcrumbItem>
-					<BreadcrumbLink href="/docs/components/breadcrumbs">
-						Breadcrumb
-					</BreadcrumbLink>
-				</BreadcrumbItem>
-			</BreadcrumbList>
-		),
-	},
+  title: 'Components/Breadcrumb',
+  component: Breadcrumb,
+  tags: ['autodocs'],
+  args: {
+    children: (
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/docs">Documentation</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/docs/components">Components</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/docs/components/breadcrumbs">
+            Breadcrumb
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    ),
+  },
 }
 
 export default meta
 type Story = StoryObj<typeof Breadcrumb>
 
 export const Default: Story = {
-	args: {},
+  args: {},
 }
 
 const CustomSeparator = (
-	<BreadcrumbSeparator>
-		<ArrowLineRightIcon />
-	</BreadcrumbSeparator>
+  <BreadcrumbSeparator>
+    <ArrowLineRightIcon />
+  </BreadcrumbSeparator>
 )
 
 export const WithCustomSeparator: Story = {
-	args: {
-		children: (
-			<BreadcrumbList>
-				<BreadcrumbItem>
-					<BreadcrumbLink href="/">Home</BreadcrumbLink>
-				</BreadcrumbItem>
-				{CustomSeparator}
-				<BreadcrumbItem>
-					<BreadcrumbLink href="/docs">Documentation</BreadcrumbLink>
-				</BreadcrumbItem>{' '}
-				{CustomSeparator}
-				<BreadcrumbItem>
-					<BreadcrumbLink href="/docs/components">Components</BreadcrumbLink>
-				</BreadcrumbItem>
-				{CustomSeparator}
-				<BreadcrumbItem>
-					<BreadcrumbLink href="/docs/components/breadcrumbs">
-						Breadcrumb
-					</BreadcrumbLink>
-				</BreadcrumbItem>
-			</BreadcrumbList>
-		),
-	},
+  args: {
+    children: (
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        {CustomSeparator}
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/docs">Documentation</BreadcrumbLink>
+        </BreadcrumbItem>{' '}
+        {CustomSeparator}
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/docs/components">Components</BreadcrumbLink>
+        </BreadcrumbItem>
+        {CustomSeparator}
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/docs/components/breadcrumbs">
+            Breadcrumb
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    ),
+  },
 }
 
 export const WithEllipsis: Story = {
-	args: {
-		children: (
-			<BreadcrumbList>
-				<BreadcrumbItem>
-					<BreadcrumbLink href="/">Breadcrumb</BreadcrumbLink>
-				</BreadcrumbItem>
-				<BreadcrumbSeparator />
-				<BreadcrumbItem>
-					<BreadcrumbEllipsis />
-				</BreadcrumbItem>
-				<BreadcrumbSeparator />
-				<BreadcrumbItem>
-					<BreadcrumbLink>WithEllipsis</BreadcrumbLink>
-				</BreadcrumbItem>
-			</BreadcrumbList>
-		),
-	},
+  args: {
+    children: (
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Breadcrumb</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbEllipsis />
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink>WithEllipsis</BreadcrumbLink>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    ),
+  },
 }
 
 export const WithDisabledLink: Story = {
-	args: {
-		children: (
-			<BreadcrumbList>
-				<BreadcrumbItem>
-					<BreadcrumbLink href="/">Breadcrumb</BreadcrumbLink>
-				</BreadcrumbItem>
-				<BreadcrumbSeparator />
-				<BreadcrumbItem>
-					<BreadcrumbLink href="/docs">Documentation</BreadcrumbLink>
-				</BreadcrumbItem>
-				<BreadcrumbSeparator />
-				<BreadcrumbItem>
-					<BreadcrumbLink href="/docs/components">Components</BreadcrumbLink>
-				</BreadcrumbItem>
-				<BreadcrumbSeparator />
-				<BreadcrumbItem>
-					<BreadcrumbLink href="/docs/components/breadcrumbs" disabled>
-						Breadcrumb
-					</BreadcrumbLink>
-				</BreadcrumbItem>
-			</BreadcrumbList>
-		),
-	},
+  args: {
+    children: (
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Breadcrumb</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/docs">Documentation</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/docs/components">Components</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/docs/components/breadcrumbs" disabled>
+            Breadcrumb
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    ),
+  },
 }
 
 // TODO: uncomment once dropdown is ready

746aa5622f599d856e8389d57e6e52d19371e751
Kirill Petunin <kpetunin@icloud.com>
2025-01-11 10:17:08 +0300

    feat: add ToggleGroup component and its stories, and update Input exports

---
+++ src/components/Input/ToggleGroup.tsx
@@ -0,0 +1,72 @@
+'use client'
+
+import * as ToggleGroupPrimitive from '@radix-ui/react-toggle-group'
+import type { VariantProps } from 'class-variance-authority'
+
+import { type ComponentProps, type FC, createContext, useContext } from 'react'
+import { twMerge } from 'tailwind-merge'
+import { toggleVariants } from './Toggle'
+
+const ToggleGroupContext = createContext<VariantProps<typeof toggleVariants>>({
+  size: 'md',
+  variant: 'borderless',
+})
+
+type ToggleGroupProps = ComponentProps<typeof ToggleGroupPrimitive.Root> &
+  VariantProps<typeof toggleVariants>
+
+const ToggleGroup: FC<ToggleGroupProps> = ({
+  className,
+  variant,
+  size,
+  children,
+  ...props
+}) => (
+  <ToggleGroupPrimitive.Root
+    className={twMerge('flex items-center justify-center gap-1', className)}
+    {...props}
+  >
+    <ToggleGroupContext.Provider value={{ variant, size }}>
+      {children}
+    </ToggleGroupContext.Provider>
+  </ToggleGroupPrimitive.Root>
+)
+
+ToggleGroup.displayName = ToggleGroupPrimitive.Root.displayName
+
+type ToggleGroupItemProps = ComponentProps<typeof ToggleGroupPrimitive.Item> &
+  VariantProps<typeof toggleVariants>
+
+const ToggleGroupItem: FC<ToggleGroupItemProps> = ({
+  className,
+  children,
+  variant,
+  size,
+  ...props
+}) => {
+  const context = useContext(ToggleGroupContext)
+
+  return (
+    <ToggleGroupPrimitive.Item
+      className={twMerge(
+        toggleVariants({
+          variant: context.variant || variant,
+          size: context.size || size,
+        }),
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </ToggleGroupPrimitive.Item>
+  )
+}
+
+ToggleGroupItem.displayName = ToggleGroupPrimitive.Item.displayName
+
+export {
+  ToggleGroup,
+  ToggleGroupItem,
+  type ToggleGroupItemProps,
+  type ToggleGroupProps,
+}
